9739e92aea881d0952f8a498760460711e2339a4
KevinSchimke <99587213+KevinSchimke@users.noreply.github.com>
2022-05-08 19:22:43 +0100

    create yourQuiz , database with questions, mediastyle

--- script/script.js
+++ script/script.js
@@ -3,6 +3,7 @@ let currentQuestion = 0;
 let rightAnswer = 0;
 let audioSuccess = new Audio('sounds/success.mp3');
 let audioFail = new Audio('sounds/fail.mp3');
+let titleQuiz = "Dein Quiz starten";
 
 function enableBtn(btnID){
     document.getElementById(btnID).disabled = false;
@@ -16,8 +17,28 @@ function init() {
     showStartScreen();
 }
 
-function showQuestion(QuizNumber) {
-    selectedQuiz = filterQuiz(QuizNumber);
+function showStartScreen(){
+    document.getElementById('content').innerHTML = "";
+    document.getElementById('content').innerHTML = templateGameMenu(titleQuiz);
+    document.getElementById('yourQuizName').innerHTML = titleQuiz;
+}
+
+function startQuiz(QuizName){
+    document.getElementById('content').innerHTML = "";
+    document.getElementById('content').innerHTML = templateQuiz(QuizName);
+    showQuestion(QuizName);
+}
+
+function filterQuiz(filterQuiz){
+    return quizContent.filter(quiz => quiz.quiz_name == filterQuiz);
+} 
+
+function showQuestion(QuizName) {
+    if (QuizName == "Dein Quiz starten") {
+        alert('Bitte erst ein Quiz erstellen');
+        createYourQuiz();
+    }
+    selectedQuiz = filterQuiz(QuizName);
     let QuizPosition = selectedQuiz[(currentQuestion)];
     updateProgressbar();
     document.getElementById('lastQuestion').innerHTML = selectedQuiz.length;
@@ -44,7 +65,6 @@ function showEndscreen() {
 }
 
 function showNewQuestion(QuizPosition) {
-    
     document.getElementById('actualQuestion').innerHTML = currentQuestion + 1;
     document.getElementById('questionText').innerHTML = QuizPosition['question'];
     document.getElementById('answer_1').innerHTML = QuizPosition['answer_1'];
@@ -78,12 +98,12 @@ function failAnswer(selectedAnswer,ID_RightAnswer) {
     audioFail.play();
 }
 
-function nextQuestion(QuizNumber) {
+function nextQuestion(QuizName) {
     currentQuestion++;
     disabledBtn('nextQuestion');
     unlockAnswerBtn();
     resetAnswerBtn();
-    showQuestion(QuizNumber);
+    showQuestion(QuizName);
 }
 
 function resetAnswerBtn() {
@@ -116,19 +136,30 @@ function unlockAnswerBtn(){
     }                                              
 }
 
-
-
-function showStartScreen(){
+function createYourQuiz(){
     document.getElementById('content').innerHTML = "";
-    document.getElementById('content').innerHTML = templateGameMenu();
+    document.getElementById('content').innerHTML = templateCreateYourQuiz();
 }
 
-function startQuiz(QuizNumber){
+function createYourQuestion(){
     document.getElementById('content').innerHTML = "";
-    document.getElementById('content').innerHTML = templateQuiz(QuizNumber);
-    showQuestion(QuizNumber);
+    document.getElementById('content').innerHTML = templateCreateYourQuestion();
 }
 
-function filterQuiz(filterQuiz){
-    return quizContent.filter(quiz => quiz.quiz_name == filterQuiz);
-} 
+function saveYourQuestion(){
+    let yourQuizQuestion = document.getElementById('inputQuizQuestion').value;
+    let yourQuizAnswer_1 = document.getElementById('inputQuizAnswer_1').value;
+    let yourQuizAnswer_2 = document.getElementById('inputQuizAnswer_2').value;
+    let yourQuizAnswer_3 = document.getElementById('inputQuizAnswer_3').value;
+    let yourQuizAnswer_4 = document.getElementById('inputQuizAnswer_4').value;
+    let yourQuizSolution = document.getElementById('inputQuizSolution').value;
+    quizContent.push({
+    "quiz_name":titleQuiz,"question":yourQuizQuestion,"answer_1":yourQuizAnswer_1,"answer_2":yourQuizAnswer_2,"answer_3":yourQuizAnswer_3,"answer_4": yourQuizAnswer_4,"right_answer": yourQuizSolution}
+                    );
+    createYourQuestion();
+}
+
+function saveTtitleQuiz(){
+    titleQuiz = document.getElementById('inputQuizName').value;
+    createYourQuestion();
+}
--- script/template.js
+++ script/template.js
@@ -1,18 +1,18 @@
-function templateGameMenu() {
+function templateGameMenu(titleQuiz) {
     return `
             <div class="gameMenu card-body">
                 <h5 class="card-title textCenter">Wähle dein Quiz</h5>
-                <button type="button" class="card w-50 p-3 quizBtn mb-2" id=""
+                <button type="button" class="card w-50 p-3 quizBtn mb-2"
                     onclick="startQuiz('Quiz_1')">Quiz 1</button>
-                <button type="button" class="card w-50 p-3 quizBtn mb-2" id=""
+                <button type="button" class="card w-50 p-3 quizBtn mb-2"
                     onclick="startQuiz('Quiz_2')">Quiz 2</button>
-                <button type="button" class="card w-50 p-3 quizBtn mb-2" id=""
-                    onclick="">Quiz 3</button>
-                <button type="button" class="card w-50 p-3 quizBtn mb-2" id=""
-                    onclick="">Dein Quiz spielen</button>
+                <button type="button" class="card w-50 p-3 quizBtn mb-2"
+                    onclick="startQuiz('Quiz_3')">Quiz 3</button>
+                <button type="button" class="card w-50 p-3 quizBtn mb-2" id="yourQuizName"
+                    onclick="startQuiz('${titleQuiz}')">Dein Quiz spielen</button>
                 <h5 class="card-title textCenter">Oder erstelle dein eigenes Quiz</h5>
                 <button type="button" class="card w-50 p-3 quizBtn" id=""
-                    onclick="">Jetzt dein eigenes Quiz erstellen</button>
+                    onclick="createYourQuiz()">Jetzt dein eigenes Quiz erstellen</button>
             </div>
     `
 }
@@ -48,5 +48,64 @@ function templateQuiz(QuizNumber){
                     <button type="button" id="nextQuestion" disabled onclick="nextQuestion('${QuizNumber}')" class="btn btn-success">Nächste Frage</button>
                 </div>
             </div>
+    `
+}
+
+function templateCreateYourQuiz(){
+    return`
+    <div class="card-body">
+    <h5 class="card-title textCenter ">Erstelle Schritt für Schritt dein Eigenes Quiz</h5>
+    <h5 class="card-title textCenter mb-4">Für deine Familie und Freunde<br></h5>
+    <p class="textCenter">Gebe bitte einen Quiz Namen ein und bestätige mit weiter.</p>
+    <form onsubmit="saveTtitleQuiz()" class="yourQuizBtn">
+        <div class="input-group mb-3 ">
+            <span class="input-group-text" id="basic-addon1">Dein Quiz Name</span>
+            <input type="text" minlength="3" required class="form-control" id="inputQuizName">
+        </div>
+        <button type="submit" class="btn btn-success">Weiter</button>
+    </form>
+</div>
+    `
+}
+
+function templateCreateYourQuestion(){
+    return `
+    <div class="card-body">
+    <h5 class="card-title textCenter ">Erstelle Schritt für Schritt dein Eigenes Quiz</h5>
+    <h5 class="card-title textCenter mb-4">Für deine Familie und Freunde<br></h5>
+    <p class="textCenter">Gebe bitte eine Frage und 4 Antwortmöglichkeiten ein und bestätige mit "Weiter".<br>
+    Zum beenden den Button "Fertig" betätigen.</p>
+    <form onsubmit="saveYourQuestion()" class="yourQuizBtn">
+        <div class="input-group mb-3 mediaInputAmount mediawidth100">
+            <span class="input-group-text inputfieldQuizInfo" id="basic-addon1">Deine Frage</span>
+            <input type="text" minlength="10" required class="form-control mediaAmountInput" id="inputQuizQuestion" placeholder="Die Frage hier eingeben">
+        </div>
+        <div class="input-group mb-1 mediaInputAmount mediawidth100">
+            <span class="input-group-text inputfieldQuizInfo" id="basic-addon1">Antwort 1</span>
+            <input type="text" minlength="1" required class="form-control mediaAmountInput" id="inputQuizAnswer_1" placeholder="Die 1. Antwortmöglichkeit hier eingeben">
+        </div>
+        <div class="input-group mb-1 mediaInputAmount mediawidth100">
+            <span class="input-group-text inputfieldQuizInfo" id="basic-addon1">Antwort 2</span>
+            <input type="text" minlength="1" required class="form-control mediaAmountInput" id="inputQuizAnswer_2" placeholder="Die 2. Antwortmöglichkeit hier eingeben">
+        </div>
+        <div class="input-group mb-1 mediaInputAmount mediawidth100">
+            <span class="input-group-text inputfieldQuizInfo" id="basic-addon1">Antwort 3</span>
+            <input type="text" minlength="1" required class="form-control mediaAmountInput" id="inputQuizAnswer_3" placeholder="Die 3. Antwortmöglichkeit hier eingebenn">
+        </div>
+        <div class="input-group mb-3 mediaInputAmount mediawidth100">
+            <span class="input-group-text inputfieldQuizInfo" id="basic-addon1">Antwort 4</span>
+            <input type="text" minlength="1" required class="form-control mediaAmountInput" id="inputQuizAnswer_4" placeholder="Die 4. Antwortmöglichkeit hier eingeben">
+        </div>
+        <div class="input-group mb-3 mediaInputAmount mediawidth100">
+            <span class="input-group-text" id="basic-addon1">Die Lösung ist Antwort Nummer</span>
+            <input type="number" min="1" max="4" value="1" required class="form-control mediaAmountInput" id="inputQuizSolution">
+        </div>
+        
+        <button type="submit" class="btn btn-success yourQuizBtnEnter mb-1">Weiter(Speichern)</button>
+    </form>
+    <div class="yourQuizBtn">
+        <button type="button" onclick="init()" class="btn btn-secondary yourQuizBtnEnter">Fertig</button>
+    </div>
+</div>
     `
 }
